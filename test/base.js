27f813632650488ec9ef2b9bd6f6c5d0c442a6e0
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-05-02 21:03:10 +0800

    add test

---
+++ test/base.js
@@ -0,0 +1,17 @@
+'use strict';
+
+                            require('chai').should();
+const expect            =   require('chai').expect;
+const request           =   require('supertest');
+const config            =   require(__dirname + '/../config/config.js');
+const apiURL            =   config.development;
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const api               = request(apiURL); // modular
+
+const headers           =   { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+const t                 =   token => { return { 'x-access-token': token } };
+
+console.log(`test server endpoint: ${apiURL}`);
+module.exports = { api, expect, headers, t };
