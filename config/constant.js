2db56b8de9b88271b8c4602e9ddb6b86c331899f
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-05-02 14:53:25 +0800

    add constant

---
+++ config/constant.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** PERMISSION **/
+exports.PERMISSION_DENIED           = 'Permission denied';
+exports.UNAUTHORIZED_REQUEST        = 'Unauthorized request';
+exports.SERVER_ERROR                = 'Server error';
+exports.DATABASE_CONN_ERROR         = 'Connection error';
+exports.SERVICE_UNAVAILABLE         = 'Service unavailable';
+exports.FORBIDDEN_REQUEST           = 'Forbidden request';
+exports.SERVER_MAINTENANCE          = 'Maintenance: Server is undergoing service upgrade. Please return later';
+
+
+/** USER */
+exports.USER_SIGNIN_FAILED          = 'Invalid email and/or password';
+exports.USER_SIGNIN_SUCCESS         = 'Signed in success';
+exports.USER_SIGNED_OUT             = 'You have logged out';
+exports.USER_CREATE_FAILED          = 'Could not create user';
+exports.USER_CREATE_SUCCESS         = 'User created';
